bf72e67583a20ff9d00d4c108ea4cd23a3755a12
unknown <matiilaed2@gmail.com>
2021-11-08 20:59:09 -0300

    Curso de introduccion a python

---
+++ Arreglos general.py
@@ -0,0 +1,27 @@
+#Vale CABE ACLARAR, los arreglos como tal no existen en python
+#pero tratare de explicar el modo de utilzarlos en este lenguaje de igual modo
+#Como se sabe y similar a la lista(esto es lo mas cercano a un arreglo en este lenguaje) se inicia declarandolo
+# AL IGUAL QUE LA LISTA INICIA EN 0
+
+#este es un arreglo del tipo vector, ya que este solo contiene una dimension
+#          0 1 2
+arreglo = [1,2,3]
+#pero que son las dimensiones?
+
+#las dimensiones son las zonas que existen dentro de un arreglo
+#Estas son generalmente tambien llamadas celdas
+#hay varios tipos de arreglos, los comunes son del tipo vector y matriz
+
+#EL ARREGLO VECTOR
+#Un arreglo vector es un tipo de vector de una sola dimension, tal como la variable arreglo declarada
+#anteriormente, esta almacena objetos pero solo al nivel de una dimension.
+
+#EL ARREGLO MATRIZ
+#Este ya difiere un tanto del arreglo vector, ya que este tipo de vector posee 2 o mas dimensiones
+#al hacer esto es capaz de almacenar una cantidad mas grandes de datos y mejor administradas.
+
+#Ejemplo
+#            0       1       2
+matriz = [[1,3,2],[6,5,4],[8,9,0]]
+
+#Te invito a que hagas tus propios programas para que vayas viendo como utilizarlos a tu antojo
---
+++ funcion recursiva.py
@@ -0,0 +1,25 @@
+#Las funciones recursivas, te recomiendo repasar antes la clase de metodos para entender mejor esta parte
+#bien, empecemos, las funciones recursivas son funciones que se llaman a si mismas, la verdad es que de
+#utilidad no poseen mucho, es mejor utilizar bucles, pero bueno, nunca esta demas aprender a 
+#utilizarlos, no?
+
+#El ejemplo es el ingreso de una password con una serie de intentos
+
+#creamos una funcion
+def login(intentos = 3):
+    if intentos > 0:#si intentos en mayor que 0
+        print("\nIntentos disponibles: {0}".format(intentos))
+        entrada_pass = input("Ingrese password: ") #aqui ingresamos cantidad de intentos
+        print("Comprobando....")
+        if entrada_pass == "pepito123": #password pepito123
+            print("Password correcta, Bienvenido <3")
+            exit(0) #Codigo de salida exitoso
+        else: #Caso de contrasena incorrecta
+            print("Password incorrecta")
+            return login(intentos - 1)#en caso de ser incorrecta vuelve a retornar login pero en este caso en vez de ser 3, seran 2 debido a intentos -1
+    else:
+        print("No te quedan mas intentos :(")
+
+login()
+
+#Te invito a que veas otros casos de funciones recursivas para entenderlos mejor
